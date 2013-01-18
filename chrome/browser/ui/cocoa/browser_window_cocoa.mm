81716e6481837d10f099e69ce48b8cd5ae4937ad
Vincent Scheib <scheib@chromium.org>
2013-01-18 14:59:59 -0800

    WIP

--- chrome/browser/ui/cocoa/browser_window_cocoa.mm
+++ chrome/browser/ui/cocoa/browser_window_cocoa.mm
@@ -598,8 +598,8 @@ void BrowserWindowCocoa::EnterFullscreenWithChrome() {
   if ([controller_ inPresentationMode])
     [controller_ exitPresentationMode];
   else
-    [controller_ enterFullscreenForURL:url
-                            bubbleType:bubble_type];
+    [controller_ enterFullscreenForURL:GURL()
+                            bubbleType:FEB_TYPE_NONE];
 }
 
 bool BrowserWindowCocoa::IsFullscreenWithChrome() {
