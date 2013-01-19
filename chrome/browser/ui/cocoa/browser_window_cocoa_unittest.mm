84c4d541da2435522f7b5d54c19270c56a16a526
Vincent Scheib <scheib@chromium.org>
2013-01-18 16:40:39 -0800

    WIP

--- chrome/browser/ui/cocoa/browser_window_cocoa_unittest.mm
+++ chrome/browser/ui/cocoa/browser_window_cocoa_unittest.mm
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/mac/mac_util.h"
 #include "base/memory/scoped_nsobject.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/string_util.h"
